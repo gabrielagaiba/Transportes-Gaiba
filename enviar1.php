91482ed53cecae2ad351020fd1a98eba657bf418
Gabriela Gaiba <gabrielagaiba@gmail.com>
2021-08-28 12:44:14 -0300

    nuevo commit

--- enviar1.php
+++ enviar1.php
@@ -1,16 +1,21 @@
 <?php
-    $nombre = $_POST ['nombre'];
+    $alto = $_POST ['alto'];
+    $ancho = $_POST ['ancho'];
+    $profundidad = $_POST ['profundidad'];
+    $peso = $_POST ['peso'];
+    $retirar = $_POST ['retirar'];
+    $entregar = $_POST ['entregar'];    
     $mail = $_POST ['email'];
     $telefono = $_POST ['telefono'];
-    $asunto = $_POST ['asunto'];
-    $mensaje = $_POST ['mensaje'];
 
     $header ='From: ' . $mail. "\r\n";
     $header .= "X-Mailer: PHP/" .phpversion() . "\r\n";
     $header .= "Mime-Version: 1.0 \r\n";
     $header .= "Content-Type: text/plain";
 
-    $mensaje_mail = "Este mensaje fue enviado por: " . $nombre . "\r\n";
+    $mensaje_mail = "Las caracteristicas del bulto son: " . $alto . , . $ancho . , . $profundidad . , . $peso . "\r\n";
+    $mensaje_mail = "Retirar de : " . $retirar . "\r\n";
+    $mensaje_mail = "Entregar en : " . $entregar . "\r\n";
     $mensaje_mail .= "Su email es: " . $mail . "\r\n";
     $mensaje_mail .= "Mensaje: " . $mensaje . "\r\n";
     $mensaje_mail .= "Enviado el " . date ('d/m/Y', time ());
