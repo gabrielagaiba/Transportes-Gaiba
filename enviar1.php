90b277d38795c971542fe1eb95df6908c7dd2a75
Gabriela Gaiba <gabrielagaiba@gmail.com>
2021-08-28 13:03:52 -0300

    .gitignore agregado al repo

--- enviar1.php
+++ enviar1.php
@@ -13,15 +13,14 @@
     $header .= "Mime-Version: 1.0 \r\n";
     $header .= "Content-Type: text/plain";
 
-    $mensaje_mail = "Las caracteristicas del bulto son: " . $alto . , . $ancho . , . $profundidad . , . $peso . "\r\n";
+    $mensaje_mail = "Las caracteristicas del bulto son: alto " . $alto . " ancho " . $ancho . " profundidad " . $profundidad . " peso " . $peso . "\r\n";
     $mensaje_mail = "Retirar de : " . $retirar . "\r\n";
     $mensaje_mail = "Entregar en : " . $entregar . "\r\n";
     $mensaje_mail .= "Su email es: " . $mail . "\r\n";
-    $mensaje_mail .= "Mensaje: " . $mensaje . "\r\n";
     $mensaje_mail .= "Enviado el " . date ('d/m/Y', time ());
 
     $para = 'ttesgaiba@gmail.com'; 
-    $asunto_mail = 'Mensaje del sitio web -' . $asunto;
+    $asunto_mail = 'Mensaje del sitio web - Cotizacion';
 
     mail($para, $asunto_mail, utf8_decode($mensaje_mail), $header);
     header("Location:index.html");
